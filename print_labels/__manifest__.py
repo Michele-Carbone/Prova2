6bc7bf6271da21cd9b6277db78ec94d69f744ce8
Michele Carbone <81500092+Michele-Carbone@users.noreply.github.com>
2023-01-26 09:52:50 +0100

    etichetta

---
+++ print_labels/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "print_labels",
+
+    'summary': """
+       prodotto""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Product',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'product'],
+
+    # always loaded
+    # NB i report vanno inseriti in fondo alla lista dei data
+    'data': [
+        # 'security/ir.model.access.csv',
+        'data/report_paperformat.xml',
+        'report/print_label_product_report.xml',
+        'report/print_etichetta.xml',
+    ],
+    # only loaded in demonstration mode
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
+}
