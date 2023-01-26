45d68080513e7322c0fc19989231982573171f5e
Michele Carbone <81500092+Michele-Carbone@users.noreply.github.com>
2023-01-26 09:51:04 +0100

    Add files via upload

---
+++ print_product_labels/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "print_product_labels",
+
+    'summary': """
+       prodotto""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Product',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'product'],
+
+    # always loaded
+    # NB i report vanno inseriti in fondo alla lista dei data
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        'data/report_paperformat.xml',
+        'report/print_label_product_report.xml',
+        'report/print_etichetta.xml',
+    ],
+    'qweb': [
+        'static/src/css/custom.css',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
